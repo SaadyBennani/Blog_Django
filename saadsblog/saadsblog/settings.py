9cae28827f9140abee33328d4d95a54d30a807d5
SaadyBennani <sbennani23@gmail.com>
2019-04-19 18:19:10 -0700

    Changed the HTML for the site.

--- saadsblog/saadsblog/settings.py
+++ saadsblog/saadsblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+from Blog.secret import SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1931cz!dv%=&d-j2^5i_lkzu()@&7&puai^1o)feahanx3ciif'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
